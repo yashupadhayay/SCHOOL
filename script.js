c2d2872efa87b4b8597ff0cfddcd9ae3d0a9a242
yashupadhayay <162331806+yashupadhayay@users.noreply.github.com>
2024-03-26 10:05:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+let menu = document.querySelector('.header .menu');
+
+document.querySelector('#menu-btn').onclick = () =>{
+   menu.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+   menu.classList.remove('active');
+}
+
+
+document.querySelectorAll('.faq .box-container .box h3').forEach(headings =>{
+   headings.onclick = () =>{
+      headings.parentElement.classList.toggle('active');
+   }
+});
+
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 3000); // Change image every 2 seconds
+}
